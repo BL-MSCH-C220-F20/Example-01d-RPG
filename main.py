509d715cca7b2d66c3616a33accc0ab20b3efebb
BL-MSCH-C220-F19 <53633703+BL-MSCH-C220-F19@users.noreply.github.com>
2020-09-07 22:51:36 -0400

    Finished for now

--- main.py
+++ main.py
@@ -23,33 +23,57 @@ def find_passage_name(game_desc, name):
             return p
     return {}
 
+def find_passage_tag(game_desc, tag):
+    for p in game_desc["passages"]:
+        if "tags" in p and tag in p["tags"]:
+            return p
+    return {}
+
 def get_random(s):
     r = s.split(",")
     try:
         return str(random.randint(int(r[0]),int(r[1])))
     except:
         pass
-    return str(0)
+    return "0"
+
+def get_either(s):
+    r = s.split(",")    
+    try:
+        return r[int(round(random.random()))]
+    except:
+        pass
+    return "0"
 
 def format_random(description):
-    m = re.search(r'\(random: \d,\d\)', description)
+    m = re.search(r'\(random:\s*\d,\d\)', description)
     while m:
-        r = re.sub(r'\(random: (\d,\d)\)',r'\1',m.group(0))
-        description = re.sub(r'\(random: \d,\d\)',get_random(r),description)
-        m = re.search(r'\(random: \d,\d\)', description)
+        r = re.sub(r'\(random:\s*(\d,\d)\)',r'\1',m.group(0))
+        description = re.sub(r'\(random:\s*\d,\d\)',get_random(r),description)
+        m = re.search(r'\(random:\s*\d,\d\)', description)
+    return description
+
+def format_either(description):
+    m = re.search(r'\(either:\s*\d,\d\)', description)
+    while m:
+        r = re.sub(r'\(either:\s*(\d,\d)\)',r'\1',m.group(0))
+        description = re.sub(r'\(either:\s*\d,\d\)',get_either(r),description)
+        m = re.search(r'\(either:\s*\d,\d\)', description)
     return description
 
 def format_set(description):
+    description = re.sub(r'\(a:\s*([^\)]*)\)',r'[\1]',description)
     v_list = re.findall("\$[^\s]+",description)
     for v in v_list:
         if v not in set_variables:
-            set_variables[v] = "0"
-    m = re.search(r'\(set: \$[^\s]+ to [^\)]+\)', description)
+            set_variables[v] = 0
+    m = re.search(r'\(set: \$[\S]+ to [^\)]+\)', description)
     while m:
-        r = re.sub(r'\(set: (\$[^\s]+) to ([^\)]+)\)',r'\1|\2',m.group(0))
-        v_parse = r.split("|")
-        
-        print(r)
+        r = re.sub(r'\(set: (\$[\S]+) to ([^\)]+)\)',r'\1|\2',m.group(0))
+        [v1,v2] = r.split("|")
+        v2 = v2.strip()
+        v2 = re.sub(r'(\$[\S]+)',r'set_variables["\1"]',v2)
+        set_variables[v1] = eval(v2)
         description = description.replace(m.group(0),"")
         m = re.search(r'\(set: \$[^\s]+ to [^\)]+\)', description)
     return description
@@ -68,14 +92,14 @@ def format_variables(description):
     v_list = re.findall("\$[^\s]+",description)
     for v in v_list:
         if v not in set_variables:
-            set_variables[v] = "0"
-    print(set_variables)
+            set_variables[v] = 0
     for s in set_variables:
-        description = description.replace(s, set_variables[s])
+        description = description.replace(s, str(set_variables[s]))
     return description
 
 def format_desc(description, current):
     description = format_random(description)
+    description = format_either(description)
     description = format_set(description)
     # if and else
     description = format_links(description)
@@ -83,6 +107,7 @@ def format_desc(description, current):
 
     #remove (display: ) command for now; we'll put it back in render()
     description = re.sub(r'\(display: \"[^"]*\"\)','',description)
+    description = description.strip()
     return description
 
 def format_display(description, current, game_desc):
@@ -121,7 +146,8 @@ def update(current, game_desc, choice):
 
 def render(current, game_desc):
     r = format_desc(current["text"], current)
-    print("\n\n" + r)
+    if r:
+        print("\n\n" + r)
     current = format_display(current["text"], current, game_desc)
     return current
 
@@ -138,10 +164,17 @@ def main():
     game_desc = load("rpg.json")
     current = find_passage_pid(game_desc, game_desc["startnode"])
     choice = ""
+    startup = find_passage_tag(game_desc, "startup")
+    if startup:
+        render(startup, game_desc)
+    header = find_passage_tag(game_desc, "header")
+    footer = find_passage_tag(game_desc, "footer")
 
     while choice != "quit" and current != {}:
         current = update(current, game_desc, choice)
+        if header: render(header, game_desc)
         current = render(current, game_desc)
+        if footer: render(footer, game_desc)
         choice = get_input(current)
 
     print("Thanks for playing!")
